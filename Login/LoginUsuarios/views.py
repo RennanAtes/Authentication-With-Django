fe3006d6a4dcd514a74bad94bfa54e9d3303e7dd
RennanAtes <RennanAugustoBR@gmail.com>
2023-03-07 13:45:59 -0300

    Recebendo os dados do formulário cadastro

--- Login/LoginUsuarios/views.py
+++ Login/LoginUsuarios/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def cadastro(request):
-    return render(request,'cadastro.html')
-
+    if request.method == "GET":
+        return render(request,'cadastro.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
 def login(request):
     return render(request,'login.html')
